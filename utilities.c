af5da3b19922563fe2ee097e416cbd8f8c7523fe
Benedict <bobuzytech@gmail.com>
2024-02-28 04:46:56 -0800

    Handle basic fuctions needed for my simple shell

--- utilities.c
+++ utilities.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - Counts the lenght of a string
- * @s: The pointer to the string
- *
- * Return: The lenght of the string
- */
-
-int _strlen(char *s)
-{
-	int n = 0;
-
-	while (*s++)
-	{
-		n++;
-	}
-
-	return (n);
-}
-
-
-/**
  * print_out - Print a string into standard output
  * @str: The pointer to the string to be printed
  *
