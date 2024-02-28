b46a62db8021c3fced0fcec2dc33ba350bc11252
Benedict <bobuzytech@gmail.com>
2024-02-28 04:44:22 -0800

    Update header file

--- main.h
+++ main.h
@@ -4,9 +4,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int _strlen(char *s);
 void print_out(char *str);
 void display_prompt(void);
+void read_input(void);
+void execute_command(char *buffer);
 
 #endif
