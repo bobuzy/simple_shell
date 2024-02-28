6ab3a91d7d472dfd4e61e54e771ed6b0ab7f2445
Benedict <bobuzytech@gmail.com>
2024-02-28 11:36:57 -0800

    Update header file

--- main.h
+++ main.h
@@ -7,11 +7,15 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <limits.h>
+
+extern char **environ;
 
 int _strlen(char *s);
 void print_out(char *str);
 void display_prompt(void);
 void read_input(void);
-void execute_command(char *buffer);
+void execute_command(char command[], char *argv[]);
+void tokenize_command(char *buffer);
 
 #endif
