baa3bf09fb72e001c338c44ac2e5387dc5f7e90b
Benedict <bobuzytech@gmail.com>
2024-02-28 11:37:30 -0800

    Handle the PATH

--- read_execute.c
+++ read_execute.c
@@ -24,6 +24,7 @@ void read_input(void)
 		if (feof(stdin))
 		{
 			print_out("\n");
+			free(buffer);
 			exit(EXIT_SUCCESS);
 		}
 		perror("getline failed");
@@ -37,53 +38,89 @@ void read_input(void)
 		return;
 	}
 
-	execute_command(buffer);
+	tokenize_command(buffer);
 
 	free(buffer);
 }
 
 
 /**
- * execute_command - Execute command from buffer
- * @buffer: The sring of arguments to be executed
+ * tokenize_command - Tokenize the command
+ * @buffer: The input command to be tokenized
  *
  * Return: Nothing
  */
 
-void execute_command(char *buffer)
+void tokenize_command(char *buffer)
 {
-	pid_t pid;
-	char *argv[20];
 	int argc = 0;
+	char command[PATH_MAX];
+	char *token, *path, *path_copy, *path_dir, *argv[20];
 
-	char *token = strtok(buffer, " \n");
-
-	if (token == NULL)
-	{
-		return;
-	}
-
-	while (token != NULL)
+	token = strtok(buffer, " \n");
+	while (token != NULL && argc < 20)
 	{
 		argv[argc++] = token;
 		token = strtok(NULL, " \n");
 	}
-
 	argv[argc] = NULL;
 
-	pid = fork();
+	if (strchr(argv[0], '/') == NULL)
+	{
+		path = getenv("PATH");
+		path_copy = strdup(path);
+		path_dir = strtok(path_copy, ":");
 
+		while (path_dir != NULL)
+		{
+			strcpy(command, "");
+			snprintf(command, sizeof(command), "%s/%s", path_dir, argv[0]);
+
+			if (access(command, X_OK) == 0)
+			{
+				execute_command(command, argv);
+				free(path_copy);
+				return;
+			}
+			path_dir = strtok(NULL, ":");
+		}
+		write(STDERR_FILENO, argv[0], strlen(argv[0]));
+		write(STDERR_FILENO, ": command not found\n", 20);
+		free(path_copy);
+	}
+	else
+	{
+		command = argv[0];
+		execute_command(command, argv);
+	}
+}
+
+
+/**
+ * execute_command - Execute command from buffer
+ * @command: An array of the command to be executed
+ * @argv: The array of the list of the arguments tp be executed
+ *
+ * Return: Nothing
+ */
+
+void execute_command(char command[], char *argv[])
+{
+	int val;
+	pid_t pid;
+
+	pid = fork();
 	if (pid == -1)
 	{
 		perror("Fork Unsuccessful");
 	}
 	else if (pid == 0)
 	{
-		int val = execve(argv[0], argv, NULL);
-
+		val = execve(command, argv, environ);
 		if (val == -1)
 		{
 			perror("Error");
+			exit(EXIT_FAILURE);
 		}
 	}
 	else
